47ed0f10088127a39056aadfde7b81dd3de8aac3
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-11-23 18:20:59 +0330

    create models MessageEndpoint

--- DjangoMessenger/message_engine/admin.py
+++ DjangoMessenger/message_engine/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import MessageEndpoint
 
-# Register your models here.
+
+admin.site.register(MessageEndpoint)
--- DjangoMessenger/message_engine/models.py
+++ DjangoMessenger/message_engine/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class MessageEndpoint(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="message_endpoints",
+    )
+
+    identifier = models.CharField(
+        max_length=255,
+        help_text="A unique identifier for this endpoint (email, phone, chat_id, token, etc.)"
+    )
+
+    endpoint_type = models.CharField(
+        max_length=100,
+        help_text="Endpoint type (e.g. sms, email, telegram, push). "
+    )
+
+    verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    priority = models.PositiveIntegerField(
+        default=100,
+        help_text="Lower value means higher priority."
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        unique_together = ("user", "identifier", "endpoint_type")
+        ordering = ["priority", "-verified", "-is_active", "endpoint_type"]
+
+    def __str__(self):
+        return f"{self.user} | {self.type}: {self.identifier}"
