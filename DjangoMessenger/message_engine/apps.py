7db1dd44288fa36bbb4d0fe933863af267b87b9f
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-11-27 00:05:38 +0330

    Auto discover message_engine_concrete and register it

--- DjangoMessenger/message_engine/apps.py
+++ DjangoMessenger/message_engine/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
+from django.utils.module_loading import autodiscover_modules
 
 
 class MessageEngineConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "message_engine"
+    
+    def ready(self):
+        autodiscover_modules('message_engine_concrete')
+
+
+"""
+Recommended structure to client's app:
+
+my_app/
+    message_engine_concrete/
+        senders.py
+        permissions.py
+        payloads.py
+        builders.py
+"""
