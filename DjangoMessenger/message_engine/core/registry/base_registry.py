9d42ed87f0cf87d0e4f0fe06926b4a4faf136af5
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-11-26 23:42:43 +0330

    Add BaseRegistry class

---
+++ DjangoMessenger/message_engine/core/registry/base_registry.py
@@ -0,0 +1,38 @@
+from typing import List, TypeVar, Dict
+from abc import ABC, abstractmethod
+
+
+ItemClass = TypeVar('item_class')
+
+
+class BaseRegistry:
+    
+    _REGISTRY: Dict[str, ItemClass] = None
+    
+    @classmethod
+    def register(cls, name: str, item_class: ItemClass):
+        name = name.strip().lower()
+        if name not in cls._REGISTRY:
+            cls._REGISTRY[name] = item_class
+    
+    @classmethod
+    def get(cls, name: str):
+        name = name.strip().lower()
+        return cls._REGISTRY.get(name, None)
+    
+    @classmethod
+    def exists(cls, name: str) -> bool:
+        name = name.strip().lower()
+        return name in cls._REGISTRY
+    
+    @classmethod
+    def list_names(cls) -> List[str]:
+        return list(cls._REGISTRY.keys())
+    
+    @classmethod
+    def list_classes(cls) -> List[ItemClass]:
+        return list(cls._REGISTRY.values())
+    
+    @classmethod
+    def clear(cls):
+        cls._REGISTRY.clear()
